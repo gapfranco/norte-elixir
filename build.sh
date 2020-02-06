a68f86eff1bea52a0f934e7a30b0165092a747b7
Gonçalo Franco <gapfranco@gmail.com>
2020-02-06 13:58:15 -0300

    :rocket: test release

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# exit on error
+set -o errexit
+
+# Initial setup
+mix deps.get --only prod
+MIX_ENV=prod mix compile
+
+# Compile assets
+# npm install --prefix ./assets
+# npm run deploy --prefix ./assets
+# mix phx.digest
+
+# Build the release and overwrite the existing release directory
+MIX_ENV=prod mix release --overwrite
+
+# for auto DB migration upon deploy
+MIX_ENV=prod mix ecto.migrate
+
--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,8 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+# use Mix.Config
+import Config
 
 database_url =
   System.get_env("DATABASE_URL") ||
@@ -32,7 +33,7 @@ config :norte, NorteWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :norte, NorteWeb.Endpoint, server: true
+config :norte, NorteWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
